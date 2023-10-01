65d6998e3b3781178ca61cf384aeb606b36bba4b
Yury <sinkevich.work@gmail.com>
2023-10-01 20:20:49 +0300

    task is done

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,11 +1,23 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> determineDishOrderRes = new ArrayList<>();
+        List<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
 
+        int step = everyDishNumberToEat-1;
+        int i = 0;
 
-        return  null;
+        while (!queue.isEmpty()) {
+            i = (i + step) % queue.size();
+            int n = queue.remove(i);
+            determineDishOrderRes.add(n);
+        }
+        return determineDishOrderRes;
     }
 }
